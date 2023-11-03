2c2ef9778b15a7eba3a3f083ab2cb2827cc40422
sucendo <samuelug.pro@gmail.com>
2023-11-03 13:53:06 +0100

    Update wargames.js

--- js/wargames.js
+++ js/wargames.js
@@ -1,50 +1,57 @@
+// JavaScript para Simulación de Guerra Termonuclear
+
 document.addEventListener("DOMContentLoaded", function () {
-  const pantalla = document.getElementById("pantalla");
-  const computadora = document.getElementById("computadora");
-  const usuario = document.getElementById("usuario");
+    const pantalla = document.getElementById("pantalla");
+    const titulo = document.getElementById("titulo");
+    const computadora = document.getElementById("computadora");
+    const usuario = document.getElementById("usuario");
 
-  const mensajes = [
-    "Conectado con WOPR ............................",
-    "Nombre de usuario:",
-    "Contraseña (mínimo 8 caracteres):",
-    "Hola, [nombre de usuario]. ¿Jugamos a algún juego?",
-  ];
+    const mensajes = [
+        "Conectado con WOPR ............................",
+        "Nombre de usuario",
+        "Contraseña (mínimo 8 caracteres)",
+        "Hola, [nombre de usuario]. ¿Jugamos a algún juego?"
+    ];
 
-  let mensajeActual = 0;
+    let mensajeActual = 0;
 
-  const mostrarMensajeComputadora = () => {
-    if (mensajeActual >= mensajes.length) {
-      return;
-    }
-
-    computadora.textContent = ""; // Limpiar el contenido de la computadora
-
-    const mensaje = mensajes[mensajeActual];
-    const tiempoEntreCaracteres = 100; // Tiempo entre caracteres en milisegundos
-
-    let i = 0;
-    const mostrarCaracter = () => {
-      if (i < mensaje.length) {
-        computadora.textContent += mensaje.charAt(i);
-        i++;
-        setTimeout(mostrarCaracter, tiempoEntreCaracteres);
-      } else {
-        mensajeActual++;
+    function mostrarMensaje() {
         if (mensajeActual < mensajes.length) {
-          usuario.innerHTML = `<input type="text" id="respuestaUsuario" placeholder="Escribe tu respuesta aquí" onkeyup="responderUsuario(event)">`;
+            titulo.textContent = mensajes[mensajeActual];
+            mensajeActual++;
+
+            if (mensajeActual === 1 || mensajeActual === 2) {
+                const input = document.createElement("input");
+                input.type = "text";
+                input.addEventListener("keydown", function (event) {
+                    if (event.key === "Enter") {
+                        mostrarMensaje();
+                    }
+                });
+                usuario.appendChild(input);
+            } else if (mensajeActual === 3) {
+                const botonSi = document.createElement("button");
+                botonSi.textContent = "Sí";
+                botonSi.addEventListener("click", function () {
+                    mensajeActual++;
+                    mostrarMensaje();
+                });
+                usuario.appendChild(botonSi);
+
+                const botonNo = document.createElement("button");
+                botonNo.textContent = "No";
+                botonNo.addEventListener("click", function () {
+                    mensajeActual = 0; // Reiniciar el juego
+                    mostrarMensaje();
+                });
+                usuario.appendChild(botonNo);
+            }
+        } else {
+            // Mensajes completos
+            titulo.textContent = "";
+            usuario.innerHTML = "";
         }
-      }
-    };
-
-    mostrarCaracter();
-  };
-
-  const responderUsuario = (event) => {
-    if (event.key === "Enter") {
-      usuario.innerHTML = ""; // Limpiar la entrada del usuario
-      mostrarMensajeComputadora();
     }
-  };
 
-  mostrarMensajeComputadora();
+    mostrarMensaje();
 });
