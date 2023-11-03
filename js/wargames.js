e093f86ccaa1fb66723ae5d254644f08760a61f0
sucendo <samuelug.pro@gmail.com>
2023-11-03 13:22:40 +0100

    Update wargames.js

--- js/wargames.js
+++ js/wargames.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const pantalla = document.getElementById("pantalla");
     const computadora = document.getElementById("computadora");
     const usuario = document.getElementById("usuario");
-
+    const titulo = document.getElementById("titulo");
     const mensajesComputadora = [
         "Conectado con WOPR ............................",
         "Nombre de usuario",
@@ -13,55 +13,57 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let mensajeActual = 0;
     let caracterActual = 0;
+    let escribiendo = false;
 
     function escribirMensaje() {
         if (mensajeActual < mensajesComputadora.length) {
             const mensaje = mensajesComputadora[mensajeActual];
             if (caracterActual < mensaje.length) {
+                usuario.innerHTML = ""; // Borra el contenido del usuario
                 computadora.textContent = mensaje.slice(0, caracterActual + 1);
                 caracterActual++;
+                escribiendo = true;
             } else {
                 // Solo avanzar al siguiente mensaje si no es el último
                 if (mensajeActual < mensajesComputadora.length - 1) {
                     mensajeActual++;
                     caracterActual = 0;
+                    escribiendo = false;
+                    setTimeout(escribirMensaje, 1000); // Espera un segundo antes de avanzar
                 }
             }
         }
     }
 
-    escribirMensaje();
-
-    function avanzarMensaje() {
+    function responder() {
         if (mensajeActual < mensajesComputadora.length - 1) {
-            mensajeActual++;
-            caracterActual = 0;
-            escribirMensaje();
+            if (escribiendo) {
+                // Si la computadora todavía está escribiendo, muestra el mensaje completo
+                usuario.textContent = mensajesComputadora[mensajeActual];
+                computadora.textContent = "";
+                mensajeActual++;
+                caracterActual = 0;
+                escribiendo = false;
+                setTimeout(escribirMensaje, 1000); // Espera un segundo antes de avanzar
+            }
         } else {
             // Mostrar opciones al final del último mensaje
             usuario.innerHTML = '<button id="si">Sí</button><button id="no">No</button>';
         }
     }
 
-    // Agregar lógica para manejar las respuestas de los botones "Sí" y "No"
-    document.getElementById("si").addEventListener("click", function () {
-        // Lógica para responder "Sí"
-        avanzarMensaje();
-    });
+    // Inicia la conversación
+    escribirMensaje();
 
-    document.getElementById("no").addEventListener("click", function () {
-        // Lógica para responder "No"
-        avanzarMensaje();
-    });
+    // Agregar lógica para manejar las respuestas de los botones "Sí" y "No"
+    document.getElementById("si").addEventListener("click", responder);
+
+    document.getElementById("no").addEventListener("click", responder);
 
     // Capturar el evento "Enter" en campos de entrada de texto
     document.addEventListener("keydown", function (event) {
-        if (event.key === "Enter" && mensajeActual === 1) {
-            // Avanzar al siguiente mensaje si el usuario presiona "Enter" en el primer campo de entrada
-            avanzarMensaje();
-        } else if (event.key === "Enter" && mensajeActual === 2 && document.getElementById("codigoUsuario").value.length >= 8) {
-            // Avanzar al siguiente mensaje si el usuario presiona "Enter" en el segundo campo de entrada (cuando la contraseña es lo suficientemente larga)
-            avanzarMensaje();
+        if (event.key === "Enter") {
+            responder();
         }
     });
 });
