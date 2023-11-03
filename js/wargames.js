6bb275574259c206cc9aeb5aa8bc3f5b9036367f
sucendo <samuelug.pro@gmail.com>
2023-11-03 14:31:55 +0100

    Update wargames.js

--- js/wargames.js
+++ js/wargames.js
@@ -1,56 +1,145 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const chat = document.getElementById("chat");
-    const mensajeInicial = document.getElementById("mensaje-inicial");
-    const userInput = document.getElementById("userInput");
-    const enviarButton = document.getElementById("enviar");
+const opciones = ["piedra", "papel", "tijeras"];
+        let victorias = 0;
+        let derrotas = 0;
+        let empates = 0;
+        let temporizador = 60;
+        let juegoTerminado = true; // Iniciar el juego como "terminado"
+        let actualizarTemporizador;
+        let defcon = 5;
+        let defconInterval;
 
-    const preguntas = [
-        "Nombre de usuario:",
-        "Contraseña (mínimo 8 caracteres):",
-        "Hola [nombre de usuario]. ¿Jugamos a algún juego?"
-    ];
+        // Array de mensajes de victoria y derrota
+        const mensajesVictoria = [
+            "¡Victoria! Has derrotado al enemigo.",
+            "¡Victoria! El enemigo se ha rendido.",
+            "¡Victoria! Tu estrategia fue impecable.",
+            "¡Victoria! El enemigo no tuvo oportunidad.",
+            "¡Victoria! Tu ejército es invencible."
+        ];
 
-    let preguntaActual = 0;
+        const mensajesDerrota = [
+            "¡Derrota! Tu capital ha sido atacado.",
+            "¡Derrota! El enemigo se alza victorioso.",
+            "¡Derrota! La guerra no ha terminado bien.",
+            "¡Derrota! Necesitas reevaluar tu estrategia.",
+            "¡Derrota! El enemigo era más poderoso."
+        ];
 
-    function mostrarMensaje(mensaje) {
-        const nuevoMensaje = document.createElement("div");
-        nuevoMensaje.textContent = mensaje;
-        chat.appendChild(nuevoMensaje);
-    }
+        function mostrarDialogoInicio() {
+            const inicioDialog = document.getElementById("inicioDialog");
+            inicioDialog.showModal();
+        }
 
-    function siguientePregunta() {
-        if (preguntaActual < preguntas.length) {
-            if (preguntaActual === 2) {
-                userInput.classList.remove("escondido");
-                enviarButton.classList.remove("escondido");
-                userInput.focus();
+        function iniciarJuego() {
+            temporizador = 60;
+            victorias = 0;
+            derrotas = 0;
+            empates = 0;
+            juegoTerminado = false; // Iniciar el juego como "no terminado"
+            actualizarTemporizador = setInterval(actualizarCuentaAtras, 1000);
+            mostrarDEFCON();
+            defconInterval = setInterval(decrementarDEFCON, 2000);
+
+            document.getElementById("iniciarJuegoButton").style.display = "none"; // Ocultar el botón "Iniciar Juego"
+            document.getElementById("temporizador").textContent = `Tiempo restante: ${temporizador} segundos`;
+            document.getElementById("resultado").textContent = "";
+            document.getElementById("puntuacion").textContent = "Victorias: 0 | Derrotas: 0 | Empates: 0";
+            mostrarCodigo("TuCodigo");
+        }
+
+        function decrementarDEFCON() {
+            defcon--;
+            if (defcon === 0) {
+                clearInterval(defconInterval);
+                document.getElementById("defcon").textContent = "DEFCON 1";
+            } else {
+                document.getElementById("defcon").textContent = `DEFCON ${defcon}`;
+            }
+        }
+
+        function mostrarDEFCON() {
+            defcon = 5;
+            defconInterval = setInterval(decrementarDEFCON, 2000);
+            document.getElementById("defcon").textContent = `DEFCON ${defcon}`;
+        }
+
+        function actualizarCuentaAtras() {
+            temporizador--;
+            document.getElementById("temporizador").textContent = `Tiempo restante: ${temporizador} segundos`;
+            if (temporizador === 0) {
+                clearInterval(actualizarTemporizador);
+                document.getElementById("temporizador").textContent = "¡Tiempo agotado!";
+                document.getElementById("iniciarJuegoButton").style.display = "block"; // Mostrar el botón "Iniciar Juego"
+                juegoTerminado = true;
+            }
+        }
+
+        function elegirOpcion(opcion) {
+            if (juegoTerminado) return;
+
+            clearInterval(defconInterval); // Reiniciar el contador DEFCON
+            mostrarDEFCON(); // Mostrar DEFCON nuevamente
+
+            const eleccionComputadora = opciones[Math.floor(Math.random() * 3)];
+
+            let resultado;
+            if (opcion === eleccionComputadora) {
+                resultado = "Empate. La batalla continúa.";
+                document.getElementById("resultado").className = "empate";
+                empates++;
+            } else if (
+                (opcion === "piedra" && eleccionComputadora === "tijeras") ||
+                (opcion === "papel" && eleccionComputadora === "piedra") ||
+                (opcion === "tijeras" && eleccionComputadora === "papel")
+            ) {
+                resultado = mensajesVictoria[Math.floor(Math.random() * mensajesVictoria.length)];
+                document.getElementById("resultado").className = "victoria";
+                victorias++;
+            } else {
+                resultado = mensajesDerrota[Math.floor(Math.random() * mensajesDerrota.length)];
+                document.getElementById("resultado").className = "derrota";
+                derrotas++;
             }
 
-            mostrarMensaje(preguntas[preguntaActual]);
-            preguntaActual++;
+            document.getElementById("resultado").textContent = `Elegiste ${opcion}, la computadora eligió ${eleccionComputadora}. ${resultado}`;
+            document.getElementById("puntuacion").textContent = `Victorias: ${victorias} | Derrotas: ${derrotas} | Empates: ${empates}`;
         }
-    }
 
-    enviarButton.addEventListener("click", function () {
-        if (preguntaActual === 0) {
-            const nombreUsuario = userInput.value.trim();
-            if (nombreUsuario.length >= 1) {
-                const pregunta = preguntas[2].replace("[nombre de usuario]", nombreUsuario);
-                mostrarMensaje(`Tú: ${nombreUsuario}`);
-                userInput.value = "";
-                userInput.classList.add("escondido");
-                enviarButton.classList.add("escondido");
-                setTimeout(function () {
-                    mostrarMensaje(pregunta);
-                    siguientePregunta();
-                }, 1000);
+        function mostrarCodigo(codigo) {
+            const codigoUsuario = document.getElementById("codigoUsuario");
+            const tiempoTotal = 60000; // 60 segundos
+            let tiempoTranscurrido = 0;
+            let codigoActual = Array(codigo.length).fill('');
+            let indices = Array.from({ length: codigo.length }, (_, i) => i);
+        
+            const interval = setInterval(() => {
+                if (tiempoTranscurrido >= tiempoTotal) {
+                    // Mostrar el código completo cuando el tiempo ha terminado
+                    clearInterval(interval);
+                    codigoUsuario.textContent = codigo;
+                    return;
+                }
+        
+                // Verificar y actualizar todas las posiciones al mismo tiempo
+                indices.forEach((index) => {
+                    if (tiempoTranscurrido >= tiempoTotal - 1000) {
+                        // Cuando quede 1 segundo, mostrar el carácter correspondiente
+                        codigoActual[index] = codigo[index];
+                    } else {
+                        if (codigoActual[index] !== codigo[index]) {
+                            codigoActual[index] = generarCaracterAleatorio(); // Si no coincide, generar un carácter aleatorio
+                        }
+                    }
+                });
+        
+                codigoUsuario.textContent = codigoActual.join('');
+                tiempoTranscurrido += 100; // Incrementar el tiempo transcurrido en 100ms
+            }, 100); // Mostrar cada 100ms
+        
+            function generarCaracterAleatorio() {
+                const caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~¡¢£¤¥¦¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ";
+                const caracterAleatorio = caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+                return caracterAleatorio;
             }
         }
-    });
-
-    setTimeout(function () {
-        mensajeInicial.classList.add("escondido");
-        mostrarMensaje("Tú: Usuario");
-        siguientePregunta();
-    }, 3000);
-});
+    </script>
