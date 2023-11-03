e7c71b942e9200a59b759c099f8d12cd7f7710d0
sucendo <samuelug.pro@gmail.com>
2023-11-03 13:50:45 +0100

    Update wargames.js

--- js/wargames.js
+++ js/wargames.js
@@ -1,74 +1,50 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const pantalla = document.getElementById("pantalla");
-    const computadora = document.getElementById("computadora");
-    const usuario = document.getElementById("usuario");
+  const pantalla = document.getElementById("pantalla");
+  const computadora = document.getElementById("computadora");
+  const usuario = document.getElementById("usuario");
 
-    const mensajesComputadora = [
-        "Conectado con WOPR ............................",
-        "Nombre de usuario",
-        "contraseña (mínimo 8 caracteres)",
-        "Hola ",
-        "¿Jugamos a algún juego?"
-    ];
+  const mensajes = [
+    "Conectado con WOPR ............................",
+    "Nombre de usuario:",
+    "Contraseña (mínimo 8 caracteres):",
+    "Hola, [nombre de usuario]. ¿Jugamos a algún juego?",
+  ];
 
-    const respuestasComputadora = [
-        "",
-        "¡Hola! Por supuesto, primero necesito tu nombre.",
-        "Ahora, por favor, ingresa una contraseña segura (mínimo 8 caracteres).",
-        "¡Hola! ¿En qué puedo ayudarte hoy?",
-        "Perfecto, ¿qué tipo de juego te gustaría jugar?"
-    ];
+  let mensajeActual = 0;
 
-    let mensajeActual = 0;
-    let caracterActual = 0;
-    let escribiendo = false;
-
-    function escribirMensaje() {
-        if (mensajeActual < mensajesComputadora.length) {
-            const mensaje = mensajesComputadora[mensajeActual];
-            if (caracterActual < mensaje.length) {
-                computadora.textContent = mensaje.slice(0, caracterActual + 1);
-                caracterActual++;
-                escribiendo = true;
-                setTimeout(escribirMensaje, 100);
-            } else {
-                if (mensajeActual === 0) {
-                    mensajeActual++; // Si es el primer mensaje, pasar al siguiente automáticamente
-                    caracterActual = 0;
-                } else if (respuestasComputadora[mensajeActual]) {
-                    usuario.textContent = respuestasComputadora[mensajeActual];
-                } else if (mensajeActual === 2) {
-                    usuario.innerHTML = 'Nombre de usuario: <input type="text" id="nombreUsuario">';
-                    document.getElementById("nombreUsuario").addEventListener("keydown", function (event) {
-                        if (event.key === "Enter") {
-                            mensajeActual++;
-                            caracterActual = 0;
-                            usuario.innerHTML = "";
-                        }
-                    });
-                } else if (mensajeActual === 3) {
-                    usuario.textContent = `Hola ${document.getElementById("nombreUsuario").value}`;
-                } else if (mensajeActual === 4) {
-                    usuario.innerHTML = '<button id="si">Sí</button><button id="no">No</button>';
-                }
-                caracterActual = 0;
-                escribiendo = false;
-                mensajeActual++;
-            }
-        }
+  const mostrarMensajeComputadora = () => {
+    if (mensajeActual >= mensajes.length) {
+      return;
     }
 
-    escribirMensaje();
+    computadora.textContent = ""; // Limpiar el contenido de la computadora
 
-    document.getElementById("si").addEventListener("click", function () {
-        if (!escribiendo) {
-            // Lógica para responder "Sí" y continuar la conversación
-        }
-    });
+    const mensaje = mensajes[mensajeActual];
+    const tiempoEntreCaracteres = 100; // Tiempo entre caracteres en milisegundos
 
-    document.getElementById("no").addEventListener("click", function () {
-        if (!escribiendo) {
-            // Lógica para responder "No" y continuar la conversación
+    let i = 0;
+    const mostrarCaracter = () => {
+      if (i < mensaje.length) {
+        computadora.textContent += mensaje.charAt(i);
+        i++;
+        setTimeout(mostrarCaracter, tiempoEntreCaracteres);
+      } else {
+        mensajeActual++;
+        if (mensajeActual < mensajes.length) {
+          usuario.innerHTML = `<input type="text" id="respuestaUsuario" placeholder="Escribe tu respuesta aquí" onkeyup="responderUsuario(event)">`;
         }
-    });
+      }
+    };
+
+    mostrarCaracter();
+  };
+
+  const responderUsuario = (event) => {
+    if (event.key === "Enter") {
+      usuario.innerHTML = ""; // Limpiar la entrada del usuario
+      mostrarMensajeComputadora();
+    }
+  };
+
+  mostrarMensajeComputadora();
 });
