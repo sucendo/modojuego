c451f84074697f5595f6aa47da670b8de7e9bb15
sucendo <samuelug.pro@gmail.com>
2023-11-03 13:20:49 +0100

    Update wargames.js

--- js/wargames.js
+++ js/wargames.js
@@ -1,10 +1,8 @@
-// Espera a que se cargue el contenido HTML
 document.addEventListener("DOMContentLoaded", function () {
     const pantalla = document.getElementById("pantalla");
     const computadora = document.getElementById("computadora");
     const usuario = document.getElementById("usuario");
 
-    // Mensajes de la computadora
     const mensajesComputadora = [
         "Conectado con WOPR ............................",
         "Nombre de usuario",
@@ -15,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let mensajeActual = 0;
     let caracterActual = 0;
-    let escribiendo = false;
 
     function escribirMensaje() {
         if (mensajeActual < mensajesComputadora.length) {
@@ -23,54 +20,48 @@ document.addEventListener("DOMContentLoaded", function () {
             if (caracterActual < mensaje.length) {
                 computadora.textContent = mensaje.slice(0, caracterActual + 1);
                 caracterActual++;
-                escribiendo = true;
-                setTimeout(escribirMensaje, 100); // Velocidad de escritura (100ms)
             } else {
-                if (mensajeActual === 1) {
-                    usuario.innerHTML = '<input type="text" id="nombreUsuario">';
-                    document.getElementById("nombreUsuario").addEventListener("keydown", function (event) {
-                        if (event.key === "Enter") {
-                            mensajeActual++;
-                            caracterActual = 0;
-                            usuario.innerHTML = `<input type="text" id="codigoUsuario">`;
-                            escribirMensaje();
-                        }
-                    });
-                } else if (mensajeActual === 2) {
-                    usuario.innerHTML = '<input type="password" id="codigoUsuario">';
-                    document.getElementById("codigoUsuario").addEventListener("keydown", function (event) {
-                        if (event.key === "Enter" && event.target.value.length >= 8) {
-                            mensajeActual++;
-                            caracterActual = 0;
-                            usuario.innerHTML = "";
-                            escribirMensaje();
-                        }
-                    });
-                } else if (mensajeActual === 3) {
-                    usuario.textContent = `Hola ${document.getElementById("nombreUsuario").value}`;
-                } else if (mensajeActual === 4) {
-                    usuario.innerHTML = '<button id="si">Sí</button><button id="no">No</button>';
+                // Solo avanzar al siguiente mensaje si no es el último
+                if (mensajeActual < mensajesComputadora.length - 1) {
+                    mensajeActual++;
+                    caracterActual = 0;
                 }
-                mensajeActual++;
-                caracterActual = 0;
-                escribiendo = false;
-                escribirMensaje();
             }
         }
     }
 
     escribirMensaje();
 
+    function avanzarMensaje() {
+        if (mensajeActual < mensajesComputadora.length - 1) {
+            mensajeActual++;
+            caracterActual = 0;
+            escribirMensaje();
+        } else {
+            // Mostrar opciones al final del último mensaje
+            usuario.innerHTML = '<button id="si">Sí</button><button id="no">No</button>';
+        }
+    }
+
     // Agregar lógica para manejar las respuestas de los botones "Sí" y "No"
     document.getElementById("si").addEventListener("click", function () {
-        if (!escribiendo) {
-            // Lógica para responder "Sí"
-        }
+        // Lógica para responder "Sí"
+        avanzarMensaje();
     });
 
     document.getElementById("no").addEventListener("click", function () {
-        if (!escribiendo) {
-            // Lógica para responder "No"
+        // Lógica para responder "No"
+        avanzarMensaje();
+    });
+
+    // Capturar el evento "Enter" en campos de entrada de texto
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Enter" && mensajeActual === 1) {
+            // Avanzar al siguiente mensaje si el usuario presiona "Enter" en el primer campo de entrada
+            avanzarMensaje();
+        } else if (event.key === "Enter" && mensajeActual === 2 && document.getElementById("codigoUsuario").value.length >= 8) {
+            // Avanzar al siguiente mensaje si el usuario presiona "Enter" en el segundo campo de entrada (cuando la contraseña es lo suficientemente larga)
+            avanzarMensaje();
         }
     });
 });
