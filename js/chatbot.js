bed79dc6b4fee7e56a725e4a79033c78c6945f1e
sucendo <samuelug.pro@gmail.com>
2023-11-06 11:42:08 +0100

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,67 +1,51 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const chat = document.getElementById("chat");
-    const userInput = document.getElementById("userInput");
-    const enviarButton = document.getElementById("enviar");
+let respuestas = {}; // Utilizamos una variable en lugar de una constante.
 
-    let respuestas = {};
+// Obtener respuestas del archivo JSON
+fetch("data/chatbotrespuestas.json") // Actualiza la ruta al archivo JSON
+    .then(response => response.json())
+    .then(data => {
+        respuestas = data;
 
-    let nombreUsuario = "";
+        const chat = document.getElementById("chat");
+        const userInput = document.getElementById("userInput");
+        const enviarButton = document.getElementById("enviar");
 
-    // Cargar las respuestas desde el archivo JSON
-    fetch('data/chatbotrespuestas.json')
-        .then(response => response.json())
-        .then(data => {
-            respuestas = data;
+        function mostrarMensaje(usuario, mensaje) {
+            const nuevoMensaje = document.createElement("div");
+            nuevoMensaje.className = usuario === "Usuario" ? "mensaje-usuario" : "mensaje-robot";
+            nuevoMensaje.textContent = `${usuario}: ${mensaje}`;
+            chat.appendChild(nuevoMensaje);
+        }
 
-            function mostrarMensaje(usuario, mensaje) {
-                const nuevoMensaje = document.createElement("div");
-                nuevoMensaje.className = usuario === "Usuario" ? "mensaje-usuario" : "mensaje-robot";
-                nuevoMensaje.textContent = `${usuario}: ${mensaje}`;
-                chat.appendChild(nuevoMensaje);
-            }
-
-            function responderPregunta(pregunta) {
-                const respuesta = respuestas[pregunta.toLowerCase()];
-                if (respuesta) {
+        function responderPregunta(pregunta) {
+            let respuestaEncontrada = false;
+            for (const palabraClave in respuestas) {
+                if (pregunta.toLowerCase().includes(palabraClave)) {
                     mostrarMensaje("Usuario", pregunta);
-                    mostrarMensaje("Robot", respuesta);
-                    if (pregunta.toLowerCase() === "nombre") {
-                        setTimeout(() => {
-                            mostrarMensaje("Robot", `Encantado de conocerte, ${nombreUsuario}! ¿En qué puedo ayudarte?`);
-                        }, 2000); // Responder con el nombre del usuario después de 2 segundos
-                    }
-                } else {
-                    mostrarMensaje("Robot", "Lo siento, no entiendo tu pregunta.");
+                    mostrarMensaje("Robot", respuestas[palabraClave]);
+                    userInput.value = "";
+                    respuestaEncontrada = true;
+                    break;
                 }
             }
+            if (!respuestaEncontrada) {
+                mostrarMensaje("Robot", "Lo siento, no entiendo tu pregunta.");
+            }
+        }
 
-            enviarButton.addEventListener("click", function () {
-                const pregunta = userInput.value;
-                if (pregunta.trim() !== "") {
-                    mostrarMensaje("Usuario", pregunta);
-                    userInput.value = "";
+        enviarButton.addEventListener("click", function () {
+            const pregunta = userInput.value.trim();
+            if (pregunta !== "") {
+                responderPregunta(pregunta);
+            }
+        });
+
+        userInput.addEventListener("keyup", function (event) {
+            if (event.key === "Enter") {
+                const pregunta = userInput.value.trim();
+                if (pregunta !== "") {
                     responderPregunta(pregunta);
                 }
-            });
-
-            userInput.addEventListener("keyup", function (event) {
-                if (event.key === "Enter") {
-                    const pregunta = userInput.value;
-                    if (pregunta.trim() !== "") {
-                        mostrarMensaje("Usuario", pregunta);
-                        userInput.value = "";
-                        responderPregunta(pregunta);
-                    }
-                }
-            });
-
-            // Pedir el nombre del usuario
-            setTimeout(() => {
-                const nombre = prompt("Hola, soy Chatbot. ¿Cómo te llamas?");
-                if (nombre) {
-                    nombreUsuario = nombre;
-                    mostrarMensaje("Robot", `Encantado de conocerte, ${nombreUsuario}! ¿En qué puedo ayudarte?`);
-                }
-            }, 1000); // Pedir el nombre después de 1 segundo
+            }
         });
-});
+    });
