3710aeb08e4db0f081b399a6bea8be325faa6fa9
sucendo <samuelug.pro@gmail.com>
2023-11-13 13:28:17 +0100

    Update ajedrez.js

--- js/ajedrez.js
+++ js/ajedrez.js
@@ -124,4 +124,7 @@
     }
   }
 
-  document.addEventListener('DOMContentLoaded', function ()
+  document.addEventListener('DOMContentLoaded', function () {
+    const juegoAjedrez = new Ajedrez();
+  });
+})();
