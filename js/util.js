4aaf23b57d418453769f365e21bfc389c4020498
sucendo <samuelug.pro@gmail.com>
2023-11-13 11:05:22 +0100

    Create util.js

---
+++ js/util.js
@@ -0,0 +1,10 @@
+// Módulo de utilidades
+(function() {
+  // Función para comprobar si un elemento existe
+  function existeElemento(elemento) {
+    return elemento !== null && elemento !== undefined;
+  }
+
+  // Exportar la función existeElemento()
+  export { existeElemento };
+})();
