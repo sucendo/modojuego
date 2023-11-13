28c037e7fea02ab79af89eb78730e4d0b9f4111f
sucendo <samuelug.pro@gmail.com>
2023-11-13 11:00:00 +0100

    Update ajedrez.js

--- js/ajedrez.js
+++ js/ajedrez.js
@@ -1,48 +1,64 @@
-// Importar la función existeElemento()
-import { existeElemento } from './js/util';
+// Módulo de ajedrez
+(function() {
+  const TABLERO_ANCHO = 8;
+  const TABLERO_ALTO = 8;
 
-const TABLERO_ANCHO = 8;
-const TABLERO_ALTO = 8;
+  const piezas = {
+    "♙": "peón",
+    "♖": "torre",
+    "♘": "caballo",
+    "♗": "alfil",
+    "♕": "reina",
+    "♔": "rey",
+  };
 
-const piezas = {
-  "♙": "peón",
-  "♖": "torre",
-  "♘": "caballo",
-  "♗": "alfil",
-  "♕": "reina",
-  "♔": "rey",
-};
-
-// Crear el tablero de ajedrez
-const tablero = crearTablero();
-
-// Dibujar el tablero de ajedrez
-dibujarTablero(tablero, document.getElementById("tablero"));
-
-function dibujarTablero(tablero, elemento) {
-  // Verificar si el elemento existe antes de intentar establecer su propiedad innerHTML
-  if (existeElemento(elemento)) {
-    elemento.innerHTML = "";
+  function crearTablero() {
+    const tablero = [];
+    for (let i = 0; i < TABLERO_ANCHO; i++) {
+      tablero[i] = [];
+      for (let j = 0; j < TABLERO_ALTO; j++) {
+        tablero[i][j] = null;
+      }
+    }
+    return tablero;
   }
 
-  for (let i = 0; i < TABLERO_ANCHO; i++) {
-    for (let j = 0; j < TABLERO_ALTO; j++) {
-      const pieza = tablero[i][j];
-      const celda = document.createElement("div");
-      celda.textContent = pieza ? piezas[pieza] : "";
-      celda.style.position = "absolute";
-      celda.style.top = (j * 64) + "px";
-      celda.style.left = (i * 64) + "px";
-      elemento.appendChild(celda);
+  function dibujarTablero(tablero, elemento) {
+    // Verificar si el elemento existe antes de intentar establecer su propiedad innerHTML
+    if (existeElemento(elemento)) {
+      elemento.innerHTML = "";
+    }
+
+    for (let i = 0; i < TABLERO_ANCHO; i++) {
+      for (let j = 0; j < TABLERO_ALTO; j++) {
+        const pieza = tablero[i][j];
+        const celda = document.createElement("div");
+        celda.textContent = pieza ? piezas[pieza] : "";
+        celda.style.position = "absolute";
+        celda.style.top = (j * 64) + "px";
+        celda.style.left = (i * 64) + "px";
+        elemento.appendChild(celda);
+      }
     }
   }
-}
 
-function moverPieza(origen, destino) {
-  // Comprobar si el movimiento es válido
-  // ...
+  function moverPieza(origen, destino) {
+    // Comprobar si el movimiento es válido
+    // ...
 
-  // Mover la pieza
-  tablero[origen][destino] = tablero[origen][origen];
-  tablero[origen][origen] = null;
-}
+    // Mover la pieza
+    const piezaOrigen = tablero[origen][origen];
+    const piezaDestino = tablero[destino][destino];
+    tablero[destino][destino] = piezaOrigen;
+    tablero[origen][origen] = null;
+    if (piezaDestino !== null) {
+      tablero[origen][origen] = piezaDestino;
+    }
+  }
+
+  // Crear el tablero de ajedrez
+  const tablero = crearTablero();
+
+  // Dibujar el tablero de ajedrez
+  dibujarTablero(tablero, document.getElementById("tablero"));
+})();
