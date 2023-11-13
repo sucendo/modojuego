29aac511b317ff6b08e5bc57bb1ecf12516b1aac
sucendo <samuelug.pro@gmail.com>
2023-11-13 13:26:12 +0100

    Update ajedrez.js

--- js/ajedrez.js
+++ js/ajedrez.js
@@ -1,4 +1,3 @@
-// ajedrez.js
 (function () {
   const piezas = {
     '♙': 'P', '♟': 'p',
@@ -9,65 +8,11 @@
     '♔': 'K', '♚': 'k'
   };
 
-  function crearTablero() {
-    const tablero = [];
-    for (let i = 0; i < 8; i++) {
-      tablero[i] = [];
-      for (let j = 0; j < 8; j++) {
-        tablero[i][j] = null;
-      }
-    }
-    for (let i = 0; i < 8; i++) {
-      tablero[1][i] = '♙';
-      tablero[6][i] = '♟';
-      tablero[0][i] = obtenerPiezaInicial(0, i);
-      tablero[7][i] = obtenerPiezaInicial(7, i);
-    }
-    return tablero;
-  }
-
-  function dibujarTablero() {
-    const tableroHTML = document.getElementById('tablero');
-    tableroHTML.innerHTML = '';
-    for (let i = 0; i < 8; i++) {
-      for (let j = 0; j < 8; j++) {
-        const celda = document.createElement('div');
-        celda.className = 'celda';
-        celda.dataset.row = i;
-        celda.dataset.col = j;
-        celda.addEventListener('dragstart', handleDragStart);
-        celda.addEventListener('dragover', handleDragOver);
-        celda.addEventListener('drop', handleDrop);
-  
-        // Aplicar colores a las casillas pares e impares
-        if ((i + j) % 2 === 0) {
-          celda.classList.add('celda-blanca');
-        } else {
-          celda.classList.add('celda-negra');
-        }
-  
-        const pieza = obtenerPiezaInicial(i, j);
-        if (pieza) {
-          // Agrega redondel con la letra de la pieza
-          const redondel = document.createElement('div');
-          redondel.className = 'redondel';
-          redondel.textContent = piezas[pieza];
-          celda.appendChild(redondel);
-        }
-  
-        tableroHTML.appendChild(celda);
-      }
-      // Agrega un salto de línea después de cada fila
-      const saltoDeLinea = document.createElement('br');
-      tableroHTML.appendChild(saltoDeLinea);
-    }
-  }
-
   function obtenerPiezaInicial(row, col) {
     // Configuración de las piezas iniciales en la posición inicial del tablero
     if (row === 1) return '♙'; // Peón blanco
     if (row === 6) return '♟'; // Peón negro
-  
+
     if (row === 0 || row === 7) {
       // Configuración de las piezas de la fila superior e inferior
       switch (col) {
@@ -91,33 +36,52 @@
           return null;
       }
     }
-  
+
     return null;
   }
 
+  function dibujarTablero() {
+    const tableroHTML = document.getElementById('tablero');
+    tableroHTML.innerHTML = '';
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        const celda = document.createElement('div');
+        celda.className = 'celda';
+        celda.dataset.row = i;
+        celda.dataset.col = j;
+        celda.addEventListener('dragover', handleDragOver);
+        celda.addEventListener('drop', handleDrop);
 
-  function moverPieza(origen, destino, piezaSeleccionada, tablero) {
-    if (
-      tablero &&
-      tablero[origen.row] &&
-      tablero[origen.row][origen.col] !== undefined &&
-      tablero[destino.row] &&
-      destino.row >= 0 &&
-      destino.row < 8 &&
-      destino.col >= 0 &&
-      destino.col < 8
-    ) {
-      const piezaOrigen = tablero[origen.row][origen.col];
-      tablero[destino.row][destino.col] = piezaOrigen;
-      tablero[origen.row][origen.col] = null;
-      dibujarTablero(); // Redibujar el tablero después de un movimiento
+        // Aplicar colores a las casillas pares e impares
+        if ((i + j) % 2 === 0) {
+          celda.classList.add('celda-blanca');
+        } else {
+          celda.classList.add('celda-negra');
+        }
+
+        const pieza = obtenerPiezaInicial(i, j);
+        if (pieza) {
+          // Agrega redondel como elemento "drag and drop"
+          const redondel = document.createElement('div');
+          redondel.className = 'redondel';
+          redondel.textContent = piezas[pieza];
+          redondel.draggable = true; // Hace la pieza arrastrable
+          redondel.addEventListener('dragstart', handleDragStart);
+          celda.appendChild(redondel);
+        }
+
+        tableroHTML.appendChild(celda);
+      }
+      // Agrega un salto de línea después de cada fila
+      const saltoDeLinea = document.createElement('br');
+      tableroHTML.appendChild(saltoDeLinea);
     }
   }
-  
+
   function handleDragStart(e) {
     e.dataTransfer.setData('text/plain', e.target.textContent);
-    e.dataTransfer.setData('row', e.target.dataset.row);
-    e.dataTransfer.setData('col', e.target.dataset.col);
+    e.dataTransfer.setData('row', e.target.parentElement.dataset.row);
+    e.dataTransfer.setData('col', e.target.parentElement.dataset.col);
   }
 
   function handleDragOver(e) {
@@ -135,27 +99,29 @@
       row: +e.target.dataset.row,
       col: +e.target.dataset.col
     };
-  
+
     // Verificar si el movimiento es válido
     if (esMovimientoValido(origen, destino, piezaSeleccionada)) {
-      moverPieza(origen, destino, piezaSeleccionada, this.tablero);
+      moverPieza(origen, destino, piezaSeleccionada);
     }
   }
 
+  function moverPieza(origen, destino, piezaSeleccionada) {
+    // Implementa la lógica de movimiento de piezas aquí
+    // ...
+    dibujarTablero(); // Redibujar el tablero después de un movimiento
+  }
+
   function esMovimientoValido(origen, destino, piezaSeleccionada) {
-    // Aquí deberías implementar la lógica para validar si el movimiento es legal para la pieza seleccionada
-    // Por ahora, siempre devolveremos true
+    // Implementa la lógica para verificar si el movimiento es válido
+    // Por ahora, siempre devuelve true
     return true;
   }
 
   class Ajedrez {
     constructor() {
-      this.tablero = crearTablero();
       dibujarTablero();
     }
   }
 
-  document.addEventListener('DOMContentLoaded', function () {
-    const juegoAjedrez = new Ajedrez();
-  });
-})();
+  document.addEventListener('DOMContentLoaded', function ()
