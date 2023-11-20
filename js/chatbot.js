cb235a613b9df0c3b007035bb63c0ba6bb851a30
sucendo <samuelug.pro@gmail.com>
2023-11-20 16:43:23 +0100

    Update chatbot.js

--- js/chatbot.js
+++ js/chatbot.js
@@ -1,3 +1,25 @@
+// Función para cargar un script de forma dinámica
+function cargarScript(nombreArchivo, callback) {
+  const script = document.createElement('script');
+  script.src = nombreArchivo;
+  script.defer = true;
+
+  // Manejar el evento de carga del script
+  script.onload = callback;
+
+  // Agregar el script al final del cuerpo del documento
+  document.body.appendChild(script);
+}
+
+// Uso de la función para cargar el script principal y el script de utilidades
+cargarScript('js/chatbot.js', function () {
+  // Código que depende del script principal
+  cargarScript('js/chatbotutil.js', function () {
+    // Código que depende del script de utilidades
+    // Aquí puedes iniciar tu aplicación después de cargar ambos scripts
+  });
+});
+
 // Función para cargar el archivo JSON de respuestas
 function cargarRespuestas() {
   // Colocar el foco en la caja de texto al cargar la página
@@ -117,99 +139,78 @@ async function buscarPalabrasClave(texto, respuestas) {
     const palabrasClave = palabraClave;
     if (palabras.includes(palabrasClave.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase())) {
       if (palabrasClave === "hora") {
-        const ahora = new Date();
-        const horaActual = `${ahora.getHours()}:${ahora.getMinutes()}`;
-        return `${respuestas[palabrasClave]} ${horaActual}`;
+		return `${respuestas[palabrasClave]}` + ` `+ calcularHoraActual(texto);
       } else if (palabrasClave === "es hoy") {
-        const ahora = new Date();
-        const opcionesFecha = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
-        const fechaYDia = ahora.toLocaleDateString("es-ES", opcionesFecha);
-        return `Hoy es ${fechaYDia}`;
+		return calcularDiaHoy(texto);
       } else if (palabraClave === "queda") {
 		return calcularTiempoRestante(texto);
       } else if (palabras.includes("cuanto es") || palabras.includes("calcula")) { 
-        const expresionMatematica = texto.replace(palabrasClave, "").trim();
-        try {
-          const resultado = math.evaluate(expresionMatematica);
-          return `${respuestas[palabrasClave]} ${resultado}`;
-        } catch (error) {
-          return "No pude resolver la operación matemática.";
-        }
+		const expresionMatematica = texto.replace(palabrasClave, "").trim();
+		try {
+		  const resultado = math.evaluate(expresionMatematica);
+		  return `${respuestas[palabrasClave]} ${resultado}`;
+		} catch (error) {
+		  return "No pude resolver la operación matemática.";
+		}
       } else if ((palabras.includes("me llamo") || palabras.includes("soy")) && !palabras.includes("como")) {
-        const palabrasClaveEncontradas = Object.keys(respuestas).filter(pc => palabras.includes(pc));
-        if (palabrasClaveEncontradas.length > 0) {
-          // Extraer el nombre del usuario del texto original
-          const posicionPalabraClave = palabras.indexOf(palabrasClaveEncontradas[0]);
-          // Obtener la parte del texto después de la palabra clave
-          const nuevoNombreUsuario = texto.substring(posicionPalabraClave + palabrasClaveEncontradas[0].length).trim();
+		const palabrasClaveEncontradas = Object.keys(respuestas).filter(pc => palabras.includes(pc));
+		if (palabrasClaveEncontradas.length > 0) {
+		  // Extraer el nombre del usuario del texto original
+		  const posicionPalabraClave = palabras.indexOf(palabrasClaveEncontradas[0]);
+		  // Obtener la parte del texto después de la palabra clave
+		  const nuevoNombreUsuario = texto.substring(posicionPalabraClave + palabrasClaveEncontradas[0].length).trim();
 
-          if (nuevoNombreUsuario) {
-            // Asignar el nombre a la variable global
-            nombreUsuario = nuevoNombreUsuario;
+		  if (nuevoNombreUsuario) {
+			// Asignar el nombre a la variable global
+			nombreUsuario = nuevoNombreUsuario;
 
-            // Verificar si el nombre es "Sucendo"
-            if (nombreUsuario.toLowerCase() === "sucendo") {
-              contextoConversacion.juegoIniciado = true; // Iniciar el juego
-	   contextoConversacion.palabraClave = "jugar";		  
-              return "Hola creador mío, ¿quieres jugar?";
+			// Verificar si el nombre es "Sucendo"
+			if (nombreUsuario.toLowerCase() === "sucendo") {
+			contextoConversacion.juegoIniciado = true; // Iniciar el juego
+			contextoConversacion.palabraClave = "jugar";		  
+			return "Hola creador mío, ¿quieres jugar?";
 	  
-            } else {
-              return `Encantado de conocerte, ${nombreUsuario}!`;
-            }
-          }
-        }
+			} else {
+			  return `Encantado de conocerte, ${nombreUsuario}!`;
+			}
+		  }
+		}
       } else if (palabras.includes("como") && palabras.includes("me") && palabras.includes("llamo")) {
-        if (nombreUsuario) {
-          return `Te llamas ${nombreUsuario}.`;
-        } else {
-          return "Lo siento, no tengo esa información. ¿Cómo te llamas?";
-        }
-      } else if (contextoConversacion.juegoIniciado && palabras.includes("sí")) {
-        // Manejar la respuesta afirmativa después de iniciar el juego
-        contextoConversacion.juegoIniciado = false; // Restablecer el estado del juego
-        return "Empezamos. ¿Quieres jugar a la Guerra Termonuclear?";
-      } else if (contextoConversacion.juegoIniciado && palabras.includes("quiero jugar")) {
-        // Verificar si el nombre de usuario es "Sucendo"
-        if (nombreUsuario.toLowerCase() === "sucendo") {
-          return "¿Ahora quieres jugar a la Guerra Termonuclear?";
-        } else {
-          contextoConversacion.juegoIniciado = false; // Restablecer el estado del juego
-          return "Lo siento, no es un simulador de juegos.";
-        }
+			if (nombreUsuario) {
+			  return `Te llamas ${nombreUsuario}.`;
+			} else {
+			  return "Lo siento, no tengo esa información. ¿Cómo te llamas?";
+			}
 	} else if (palabras.includes(normalizarTexto("adivinanza"))) {
-	  contextoConversacion.palabraClave = "adivinanza";
-	  // Aquí puedes mostrar la adivinanza al usuario
-	  mostrarMensaje("Robot", respuestas["adivinanza"][0]);
+		  contextoConversacion.palabraClave = "adivinanza";
+		  // Aquí puedes mostrar la adivinanza al usuario
+		  mostrarMensaje("Robot", respuestas["adivinanza"][0]);
       } else if (palabras.includes("guardar")) {
-        const palabras = texto.split(" ");
-        const datoIndex = palabras.indexOf("guardar");
+			const palabras = texto.split(" ");
+			const datoIndex = palabras.indexOf("guardar");
 
-        if (datoIndex !== -1 && datoIndex < palabras.length - 1) {
-          const clave = palabras[datoIndex + 1];
-          if (clave) {
-            // Genera una clave única
-            const dato = palabras[datoIndex + 2];
-            datosTemporales[clave] = dato;
-            return `He guardado "${dato}" temporalmente con la clave "${clave}".`;
-          }
-        }
+			if (datoIndex !== -1 && datoIndex < palabras.length - 1) {
+			  const clave = palabras[datoIndex + 1];
+			  if (clave) {
+				// Genera una clave única
+				const dato = palabras[datoIndex + 2];
+				datosTemporales[clave] = dato;
+				return `He guardado "${dato}" temporalmente con la clave "${clave}".`;
+			  }
+			}
       } else if (palabras.includes("mostrar")) {
-        const palabras = texto.split(" ");
-        const datoIndex = palabras.indexOf("mostrar");
+			const palabras = texto.split(" ");
+			const datoIndex = palabras.indexOf("mostrar");
 
-        if (datoIndex !== -1 && datoIndex < palabras.length - 1) {
-          const clave = palabras[datoIndex + 1];
-          const dato = datosTemporales[clave];
-          if (dato) {
-            return `El dato almacenado con la clave "${clave}" es: "${dato}".`;
-          } else {
-            return "No se ha encontrado ningún dato almacenado con la clave especificada.";
-          }
-        }
-      } else if (palabras.includes("película favorita")) {
-        // Establecer el contexto para gestionar la respuesta
-        contextoConversacion.palabraClave = "película favorita";
-        return respuestas["película_favorita"];
+			if (datoIndex !== -1 && datoIndex < palabras.length - 1) {
+			  const clave = palabras[datoIndex + 1];
+			  const dato = datosTemporales[clave];
+			  if (dato) {
+				return `El dato almacenado con la clave "${clave}" es: "${dato}".`;
+			  } else {
+				return "No se ha encontrado ningún dato almacenado con la clave especificada.";
+			  }
+			}
       } else if (palabraClave in respuestas) {
         // Aquí, aseguramos que siempre se elija la única respuesta si solo hay una
         const respuestasCategoria = respuestas[palabraClave];
@@ -247,58 +248,6 @@ async function buscarPalabrasClave(texto, respuestas) {
   }
 
   return "Lo siento, no entiendo tu pregunta.";
-}
-
-
-// Función para calcular el tiempo que queda para una fecha y hora específicas
-function calcularTiempoRestante(texto) {
-  // Expresión regular mejorada para buscar patrones de fecha y hora
-  const patronFechaHora = /(\d{1,2}\/\d{1,2}\/\d{4})\s*(\d{1,2}h:\d{1,2})?/i;
-  const coincidenciasFechaHora = texto.match(patronFechaHora);
-
-  if (coincidenciasFechaHora) {
-    const fecha = coincidenciasFechaHora[1];
-    const hora = coincidenciasFechaHora[2];
-
-    if (fecha || hora) {
-      // Calcular tiempo que queda para la fecha y hora especificadas
-      const ahora = new Date();
-      let fechaEspecifica = null;
-
-      if (fecha) {
-        const [dia, mes, anio] = fecha.split('/').map(Number);
-        fechaEspecifica = new Date(anio, mes - 1, dia); // Meses en JavaScript van de 0 a 11
-      }
-
-      if (hora) {
-        const [hora, minutos] = hora.split(':').map(Number);
-        if (!fechaEspecifica) {
-          // Si no se especificó la fecha, usar la fecha actual
-          fechaEspecifica = new Date();
-        }
-        fechaEspecifica.setHours(hora, minutos);
-      }
-
-      const tiempoRestante = fechaEspecifica - ahora;
-
-      if (tiempoRestante > 0) {
-        const dias = Math.floor(tiempoRestante / (1000 * 60 * 60 * 24));
-        const horas = Math.floor((tiempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutos = Math.floor((tiempoRestante % (1000 * 60 * 60)) / (1000 * 60));
-
-        if (dias > 365) {
-          const anos = Math.floor(dias / 365);
-          const diasRestantes = dias % 365;
-          return `Quedan ${anos} años, ${diasRestantes} días, ${horas} horas y ${minutos} minutos para la fecha especificada.`;
-        } else {
-          return `Quedan ${dias} días, ${horas} horas y ${minutos} minutos para la fecha especificada.`;
-        }
-      } else {
-        return `La fecha y hora especificadas ya han pasado.`;
-      }
-    }
-  }
-  return "No se proporcionó una fecha y hora válidas.";
 }
 
 // Función para mostrar mensajes en el chat
