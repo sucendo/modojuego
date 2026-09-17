e474efc1185b6c7ba5b82dd28c645b97f16035a2
sucendo <samuelug.pro@gmail.com>
2026-09-17 12:33:17 +0200

    Delete sonusarca directory

--- sonusarca/sonusarca-config.js
+++
@@ -1,7 +0,0 @@
-// SonusArca v8.1 · distribución fija para GitHub Pages
-// Sitio: https://sucendo.github.io/modojuego/sonusarca/
-// Repositorio: https://github.com/sucendo/modojuego
-window.SONUSARCA_CONFIG = {
-  githubRepo: "sucendo/modojuego",
-  apkAsset: "SonusArca.apk"
-};
