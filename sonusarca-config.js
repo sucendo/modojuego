ed198416463ab3bec1804773228f84454b976c78
sucendo <samuelug.pro@gmail.com>
2026-09-17 12:36:36 +0200

    Add files via upload

---
+++ sonusarca-config.js
@@ -0,0 +1,6 @@
+// SonusArca v8.1 · distribución fija para sucendo/modojuego
+window.SONUSARCA_CONFIG = {
+  githubRepo: "sucendo/modojuego",
+  releaseTag: "sonusarca-latest",
+  apkAsset: "SonusArca.apk"
+};
