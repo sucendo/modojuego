b84a8c5e7e1f6bc5bbd625604a5d9583f0b35bbf
sucendo <samuelug.pro@gmail.com>
2026-09-17 11:14:59 +0200

    Add files via upload

---
+++ sonusarca/README_GITHUB_PAGES.txt
@@ -0,0 +1,10 @@
+SONUSARCA v8.1
+==============
+Ubicación prevista:
+https://sucendo.github.io/modojuego/sonusarca/
+
+Repositorio:
+https://github.com/sucendo/modojuego
+
+APK esperado en la última Release:
+SonusArca.apk
---
+++ sonusarca/sonusarca-config.js
@@ -0,0 +1,7 @@
+// SonusArca v8.1 · distribución fija para GitHub Pages
+// Sitio: https://sucendo.github.io/modojuego/sonusarca/
+// Repositorio: https://github.com/sucendo/modojuego
+window.SONUSARCA_CONFIG = {
+  githubRepo: "sucendo/modojuego",
+  apkAsset: "SonusArca.apk"
+};
