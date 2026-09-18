6f759741150563080cb4846a473c5cc65a384e5f
sucendo <samuelug.pro@gmail.com>
2026-09-18 10:50:43 +0200

    Delete sonusarca-config.js

--- sonusarca-config.js
+++
@@ -1,6 +0,0 @@
-// SonusArca v8.1 · distribución fija para sucendo/modojuego
-window.SONUSARCA_CONFIG = {
-  githubRepo: "sucendo/modojuego",
-  releaseTag: "sonusarca-latest",
-  apkAsset: "SonusArca.apk"
-};
